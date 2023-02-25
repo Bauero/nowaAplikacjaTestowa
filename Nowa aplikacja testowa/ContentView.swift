ceff6b4a608a9bd5a17340e52a5517f074fec06d
Piotr Bauer <piotr.bauer.konto@gmail.com>
2023-02-25 21:30:38 +0100

    initial commit

---
+++ Nowa aplikacja testowa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nowa aplikacja testowa
+//
+//  Created by Piotr Bauer on 25/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nowa aplikacja testowa/Nowa_aplikacja_testowaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nowa_aplikacja_testowaApp.swift
+//  Nowa aplikacja testowa
+//
+//  Created by Piotr Bauer on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nowa_aplikacja_testowaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
