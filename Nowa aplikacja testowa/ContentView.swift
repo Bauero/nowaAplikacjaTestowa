1d9a4a37da227320d16dc78df4dcd461986c4316
Piotr Bauer <piotr.bauer.konto@gmail.com>
2023-02-26 15:12:22 +0100

    backoup

--- Nowa aplikacja testowa/ContentView.swift
+++ Nowa aplikacja testowa/ContentView.swift
@@ -10,14 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "hammer")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello there, \n programmer!")
         }
         .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
